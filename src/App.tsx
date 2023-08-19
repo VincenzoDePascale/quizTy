f4310d5825234bd4b6a4e820b2e27ad95f419dde
Vincenzo De Pascale <119123124+VincenzoDePascale@users.noreply.github.com>
2023-08-19 20:10:21 +0200

    .
    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {useState} from 'react';
 import './App.css'
+//components
 
-function App() {
-  const [count, setCount] = useState(0)
+//types
+import { fetchQuizQuestions, QuestionState, Difficulty } from './API';
+import { QuestionCard } from './components/QuestionCard';
+
+//style
+import { GlobalStyle, Wrapper } from './App.styles.ts';
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+console.log(questions)
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if(!gameOver){
+      const answer = e.currentTarget.value;
+      const correct = questions[number].correct_answer === answer;
+      if(correct) setScore(prev => prev + 1);
+      const AnswerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer : questions[number].correct_answer
+      };
+      setUserAnswers((prev) => [...prev, AnswerObject]);
+    }
+  }
+
+  const nextQuestion = () => {
+    const nextQuestion = number +1;
+
+    if(nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    }else{
+      setNumber(nextQuestion);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <GlobalStyle />
+      <Wrapper>
+      <h1>QuizTy</h1>
+
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+      <button className='start' onClick={startTrivia}>start</button>
+      ): null}
+
+      {!gameOver ? <p className='score'>Score: {score}</p> :null }
+
+      {loading && <p>Loading question...</p> }
+      
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+      <button className='next' onClick={nextQuestion}>Next Question</button>
+      ): null}
+      </Wrapper>
     </>
   )
 }
